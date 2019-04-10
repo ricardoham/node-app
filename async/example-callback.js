f180df1c2668a2ed68d6d936d0ab554cc8ade042
Ricardo Manoel <rdomanoel@gmail.com>
2019-04-10 00:13:46 -0300

    add location file

--- async/example-callback.js
+++ async/example-callback.js
@@ -1,6 +1,17 @@
 const weather = require('./weather');
+const location = require('./location.js');
 
 weather(function (currentWeather) {
   console.log(currentWeather);
 });
+
+location(function (location) {
+  if (!location) {
+    console.log('Unable to guess location');
+    return;
+  }
+
+  console.log('City: ' + location.city);
+  console.log('log/lat' + location.loc);
+})
 
