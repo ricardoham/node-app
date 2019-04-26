fa543abddd74991e9847a1cef2099ba556cde563
Ricardo Manoel <rdomanoel@gmail.com>
2019-04-25 23:42:06 -0300

    add some static data for get

--- project01/server.js
+++ project01/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const todos = [
+  {
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false,
+  },
+  {
+    id: 2,
+    description: 'Go to Market',
+    completed: false,
+  },
+  {
+    id: 3,
+    description: 'Go to GYM',
+    completed: true,
+  }
+];
 
 app.get('/', (req, res) => {
   res.send('Todo API Root')
+});
+
+// GET todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
 });
 
 app.listen(PORT, () => {
